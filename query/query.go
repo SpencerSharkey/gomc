435a1077609de581d6b0bb0a99e0e4fbd2daf859
Spencer Sharkey <spencer@sf-n.com>
2017-11-05 13:28:09 -0500

    organize query package

--- query/query.go
+++ query/query.go
@@ -6,7 +6,6 @@
 package query
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/binary"
 	"errors"
@@ -53,16 +52,10 @@ type Request struct {
 	sesssionID  int32
 }
 
-// ReadWithDeadline will read from our socket with a specified timeout
-func (req *Request) ReadWithDeadline(length int, timeout time.Duration) ([]byte, error) {
-	res := make([]byte, length)
-	req.con.SetDeadline(time.Now().Add(timeout * time.Second))
-	bytes, err := req.con.Read(res)
-	req.con.SetDeadline(time.Time{})
-	if bytes == 0 || err != nil {
-		return nil, errors.New("timeout of " + strconv.Itoa(int(timeout)) + "s exceeded when reading from server")
-	}
-	return res, nil
+// NewRequest - Query request factory
+func NewRequest() *Request {
+	req := &Request{}
+	return req
 }
 
 // Connect initiates a new connection to the Minecraft server host
@@ -114,80 +107,26 @@ func (req *Request) GetChallengeToken() (int32, error) {
 	return int32(challengeToken), nil
 }
 
-// A simple scanner func to read our null-byte delimited data
-func scanDelimittedResponse(input []byte, eof bool) (adv int, token []byte, err error) {
-	i := bytes.Index(input, []byte{0x00})
-	return i + 1, input[:i], nil
-}
-
-// Simple - Make a simple query request
-func (req *Request) Simple() (*SimpleResponse, error) {
-	response := &SimpleResponse{}
-
-	if req.con == nil {
-		return response, errors.New("no connection, call Request.Connect first")
+// ReadWithDeadline will read from our socket with a specified timeout
+func (req *Request) ReadWithDeadline(length int, timeout time.Duration) ([]byte, error) {
+	res := make([]byte, length)
+	req.con.SetDeadline(time.Now().Add(timeout * time.Second))
+	bytes, err := req.con.Read(res)
+	req.con.SetDeadline(time.Time{})
+	if bytes == 0 || err != nil {
+		return nil, errors.New("timeout of " + strconv.Itoa(int(timeout)) + "s exceeded when reading from server")
 	}
-
-	challengeToken, err := req.GetChallengeToken()
-	if err != nil {
-		return nil, err
-	}
-
-	// Build simple query request packet
-	buf := &bytes.Buffer{}
-	buf.Write(*magicHeader)
-	buf.WriteByte(0x00) // Packet Type 0x00 = Query Request
-	binary.Write(buf, binary.BigEndian, req.sesssionID)
-	binary.Write(buf, binary.BigEndian, challengeToken)
-
-	req.con.Write(buf.Bytes())
-
-	// Read and parse query data
-	req.con.SetReadDeadline(time.Now().Add(req.readTimeout * time.Millisecond))
-
-	reader := bufio.NewReader(req.con)
-	reader.Discard(5) // Discard header data
-	scan := bufio.NewScanner(reader)
-	scan.Split(scanDelimittedResponse)
-
-	scan.Scan()
-	response.MOTD = scan.Text()
-
-	scan.Scan()
-	response.GameType = scan.Text()
-
-	scan.Scan()
-	response.Map = scan.Text()
-
-	scan.Scan()
-	response.NumPlayers, err = strconv.Atoi(scan.Text())
-	if err != nil {
-		return nil, errors.New("error parsing numplayers field")
-	}
-
-	scan.Scan()
-	response.MaxPlayers, err = strconv.Atoi(scan.Text())
-	if err != nil {
-		return nil, errors.New("error parsing maxplayers field")
-	}
-
-	scan.Scan()
-	portAndIP := scan.Bytes()
-	response.HostPort = int16(binary.LittleEndian.Uint16(portAndIP[:2]))
-	response.HostIP = string(portAndIP[2:])
-	req.con.SetReadDeadline(time.Time{})
-
-	return response, nil
-}
-
-// NewRequest - Query request factory
-func NewRequest() *Request {
-	req := &Request{}
-	return req
+	return res, nil
 }
 
 // GenerateSessionID - Generates a 32-bit SessionID
 func GenerateSessionID() int32 {
 	rand.Seed(time.Now().UTC().UnixNano())
 	return rand.Int31() & 0x0F0F0F0F
+}
+
+// A simple scanner func to read our null-byte delimited data
+func scanDelimittedResponse(input []byte, eof bool) (adv int, token []byte, err error) {
+	i := bytes.Index(input, []byte{0x00})
+	return i + 1, input[:i], nil
 }
---
+++ query/simple.go
@@ -0,0 +1,75 @@
+/*
+	Full implementation of Minecraft query-protocol client
+	protocol spec: http://wiki.vg/Query
+*/
+
+package query
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"strconv"
+	"time"
+)
+
+// Simple - Make a simple query request
+func (req *Request) Simple() (*SimpleResponse, error) {
+	response := &SimpleResponse{}
+
+	if req.con == nil {
+		return response, errors.New("no connection, call Request.Connect first")
+	}
+
+	challengeToken, err := req.GetChallengeToken()
+	if err != nil {
+		return nil, err
+	}
+
+	// Build simple query request packet
+	buf := &bytes.Buffer{}
+	buf.Write(*magicHeader)
+	buf.WriteByte(0x00) // Packet Type 0x00 = Query Request
+	binary.Write(buf, binary.BigEndian, req.sesssionID)
+	binary.Write(buf, binary.BigEndian, challengeToken)
+
+	req.con.Write(buf.Bytes())
+
+	// Read and parse query data
+	req.con.SetReadDeadline(time.Now().Add(req.readTimeout * time.Millisecond))
+
+	reader := bufio.NewReader(req.con)
+	reader.Discard(5) // Discard header data
+	scan := bufio.NewScanner(reader)
+	scan.Split(scanDelimittedResponse)
+
+	scan.Scan()
+	response.MOTD = scan.Text()
+
+	scan.Scan()
+	response.GameType = scan.Text()
+
+	scan.Scan()
+	response.Map = scan.Text()
+
+	scan.Scan()
+	response.NumPlayers, err = strconv.Atoi(scan.Text())
+	if err != nil {
+		return nil, errors.New("error parsing numplayers field")
+	}
+
+	scan.Scan()
+	response.MaxPlayers, err = strconv.Atoi(scan.Text())
+	if err != nil {
+		return nil, errors.New("error parsing maxplayers field")
+	}
+
+	scan.Scan()
+	portAndIP := scan.Bytes()
+	response.HostPort = int16(binary.LittleEndian.Uint16(portAndIP[:2]))
+	response.HostIP = string(portAndIP[2:])
+	req.con.SetReadDeadline(time.Time{})
+
+	return response, nil
+}
