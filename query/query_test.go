8d142d8de18694d0385e69e1c17d588cd224de0c
Spencer Sharkey <spencer@sf-n.com>
2017-11-05 21:26:33 -0500

    wait for pseudo server to be avaialble before testing

--- query/query_test.go
+++ query/query_test.go
@@ -16,13 +16,17 @@ func checkFatalErr(t *testing.T, err error) {
 
 func TestSimpleQuery(t *testing.T) {
 
-	go startQueryServer()
+	serverOpen := make(chan int)
+
+	go startQueryServer(serverOpen)
+
+	// Wait for our server to be listening
+	<-serverOpen
 
 	req := NewRequest()
 
 	err := req.Connect("127.0.0.1:24565")
 	checkFatalErr(t, err)
-
 	res, err := req.Simple()
 	checkFatalErr(t, err)
 
@@ -35,7 +39,6 @@ func TestSimpleQuery(t *testing.T) {
 		HostPort:   25565,
 		HostIP:     "127.0.0.1",
 	}
-
 	if !reflect.DeepEqual(validResponse, res) {
 		t.Fatal("Simple query response invalid!", "\nexpected:\n\t", validResponse, "\nresult:\n\t", res)
 	}
@@ -47,56 +50,56 @@ func fatalServerError(err error) {
 	}
 }
 
-func startQueryServer() {
+func startQueryServer(serverOpen chan int) {
 	// Simple MC query emulator (server)
-	go func() {
-		addr, err := net.ResolveUDPAddr("udp4", "127.0.0.1:24565")
-		fatalServerError(err)
+	addr, err := net.ResolveUDPAddr("udp4", "127.0.0.1:24565")
+	fatalServerError(err)
 
-		serv, err := net.ListenUDP("udp", addr)
-		fatalServerError(err)
+	serv, err := net.ListenUDP("udp", addr)
+	fatalServerError(err)
 
-		defer serv.Close()
+	defer serv.Close()
 
-		for {
-			buf := make([]byte, 16)
-			n, clientAddr, err := serv.ReadFromUDP(buf)
-			msg := buf[:n]
+	serverOpen <- 1
 
-			// Validate magic bytes
-			if bytes.Compare(msg[:2], []byte{0xFE, 0xFD}) == 0 {
-				msgType := msg[2]
-				msgSessionID := msg[2:6]
-				if msgType == 0x09 {
-					// challenge token response
-					payload := &bytes.Buffer{}
-					payload.WriteByte(0x09)
-					payload.Write(msgSessionID)
-					payload.WriteString("1234")
-					payload.WriteByte(0x00)
-					_, err = serv.WriteToUDP(payload.Bytes(), clientAddr)
-					fatalServerError(err)
-				} else if msgType == 0x00 {
-					payload := &bytes.Buffer{}
-					payload.WriteByte(0x00)
-					payload.Write(msgSessionID)
-					parts := []string{
-						"A Minecraft Server",
-						"SMP",
-						"world",
-						"2",
-						"20",
-						"\xDD\x63127.0.0.1",
-					}
-					for _, part := range parts {
-						payload.WriteString(part)
-						payload.WriteByte(0x00)
-					}
-					_, err = serv.WriteToUDP(payload.Bytes(), clientAddr)
-					fatalServerError(err)
+	for {
+		buf := make([]byte, 16)
+		n, clientAddr, err := serv.ReadFromUDP(buf)
+		msg := buf[:n]
+
+		// Validate magic bytes
+		if bytes.Compare(msg[:2], []byte{0xFE, 0xFD}) == 0 {
+			msgType := msg[2]
+			msgSessionID := msg[2:6]
+			if msgType == 0x09 {
+				// challenge token response
+				payload := &bytes.Buffer{}
+				payload.WriteByte(0x09)
+				payload.Write(msgSessionID)
+				payload.WriteString("1234")
+				payload.WriteByte(0x00)
+				_, err = serv.WriteToUDP(payload.Bytes(), clientAddr)
+				fatalServerError(err)
+			} else if msgType == 0x00 {
+				payload := &bytes.Buffer{}
+				payload.WriteByte(0x00)
+				payload.Write(msgSessionID)
+				parts := []string{
+					"A Minecraft Server",
+					"SMP",
+					"world",
+					"2",
+					"20",
+					"\xDD\x63127.0.0.1",
 				}
+				for _, part := range parts {
+					payload.WriteString(part)
+					payload.WriteByte(0x00)
+				}
+				_, err = serv.WriteToUDP(payload.Bytes(), clientAddr)
+				fatalServerError(err)
 			}
-			fatalServerError(err)
 		}
-	}()
+		fatalServerError(err)
+	}
 }
